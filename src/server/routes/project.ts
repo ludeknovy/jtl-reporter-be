ae0dcafdbb4e11d7fa2853831c00b6ebf2f55803
Luděk Nový <ludek@Ludek-MacBook-Pro.local>
2019-12-08 20:53:53 +0100

    fix

--- src/server/routes/project.ts
+++ src/server/routes/project.ts
@@ -8,6 +8,7 @@ import { getProjectsController } from '../controllers/project/get-projects-contr
 import { getLatestItemsControllers } from '../controllers/project/get-latest-items-controllers';
 import { deleteProjectController } from '../controllers/project/delete-project-controller';
 import { updateProjectController } from '../controllers/project/update-project-controller';
+import { getProjectStatsController } from '../controllers/project/get-projects-stats-controller';
 
 export class ProjectRoutes {
   public routes(app: express.Application): void {
@@ -23,7 +24,7 @@ export class ProjectRoutes {
       .get(wrapAsync(async (req: Request, res: Response, next: NextFunction) => await getLatestItemsControllers(req, res, next)));
 
     app.route('/api/projects/overall-stats')
-      .get(wrapAsync(async (req: Request, res: Response, next: NextFunction) => await getProjectsController(req, res, next)));
+      .get(wrapAsync(async (req: Request, res: Response, next: NextFunction) => await getProjectStatsController(req, res, next)));
 
     app.route('/api/projects/:projectName')
       .delete(
