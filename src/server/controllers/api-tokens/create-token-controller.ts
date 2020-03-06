bd293526c15003985c5fa303693d4ee7ec0de65e
Luděk Nový <ludek@Ludek-MacBook-Pro.local>
2020-03-06 13:02:01 +0100

    update

--- src/server/controllers/api-tokens/create-token-controller.ts
+++ src/server/controllers/api-tokens/create-token-controller.ts
@@ -7,7 +7,7 @@ import { IGetUserAuthInfoRequest } from '../../middleware/request.model';
 export const createTokenController = async (req: IGetUserAuthInfoRequest, res: Response, next: NextFunction) => {
   const { description } = req.body;
   const { userId } = req.user;
-  const newToken = `at-${uuid()}`
+  const newToken = `at-${uuid()}`;
   try {
     await db.query(createNewApiToken(newToken, description, userId));
     return res.status(201).send({ token: newToken });
