9c02cb21ab4b290a19a4fe280cc94b2e58dd09aa
Luděk Nový <ludek@Ludek-MacBook-Pro.local>
2020-03-17 21:32:21 +0100

    role info

--- src/server/queries/users.ts
+++ src/server/queries/users.ts
@@ -1,6 +1,6 @@
 export const getUsers = () => {
   return {
-    text: `SELECT username, users.id, users.create_date as "createDate", COUNT(token.id) as "tokenCount" FROM jtl.users as users
+    text: `SELECT username, users.id, users.role, users.create_date as "createDate", COUNT(token.id) as "tokenCount" FROM jtl.users as users
     LEFT JOIN jtl.api_tokens as token on users.id = token.created_by
     GROUP BY username, users.id;`,
   };
