2fa81f85ef1896dc07859d30b3c7ab7e081887de
Luděk Nový <ludek@Ludek-MacBook-Pro.local>
2020-03-22 20:10:44 +0100

    wip

--- src/server/controllers/item/create-item-controller.ts
+++ src/server/controllers/item/create-item-controller.ts
@@ -1,19 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
 import { ItemStatus, ItemDataType } from '../../queries/items.model';
-import { prepareDataForSavingToDb } from '../../data-stats/prepare-data';
+import { prepareDataForSavingToDb, InputData, OutputData, normalizeData } from '../../data-stats/prepare-data';
 import { db } from '../../../db/db';
 import { createNewItem, saveItemStats, saveKpiData, savePlotData, saveData } from '../../queries/items';
 import { chunkData } from '../../data-stats/chunk-data';
 import * as multer from 'multer';
 import * as boom from 'boom';
 import * as fs from 'fs';
-import * as csv from 'csvtojson';
+// import * as csv from 'csvtojson';
 import * as parser from 'xml2json';
+import * as csv from 'fast-csv';
+import pg = require('pg-promise/typescript/pg-subset');
+
 
 const upload = multer(
   {
     dest: `./uploads`,
-    limits: { fieldSize: 25 * 1024 * 1024 }
+    limits: { fieldSize: 2048 * 1024 * 1024 }
   }).fields([
     { name: 'kpi', maxCount: 1 },
     { name: 'errors', maxCount: 1 },
@@ -43,56 +46,80 @@ export const createItemController = (req: Request, res: Response, next: NextFunc
     }
     try {
       const kpiFilename = kpi[0].path;
-      fileContent = await csv().fromFile(kpiFilename);
-      fs.unlinkSync(kpiFilename);
+      let tempBuffer = [];
+      const csvStream = fs.createReadStream(kpiFilename)
+        .pipe(csv.parse({ headers: true }))
+        .on('data', async row => {
+          if (tempBuffer.length === (5000 - 1)) {
+            csvStream.pause();
+            await db.query('INSERT INTO jtl.data_chunks(item_data) VALUES($1)', [JSON.stringify(tempBuffer)]);
+            tempBuffer = [];
+            csvStream.resume();
+          }
+          tempBuffer.push(normalizeData(row));
+          return;
+        })
+        .on('end', (rowCount: number) => {
+          console.log(tempBuffer.length);
+          console.log(`Parsed ${rowCount} rows`);
+          fs.unlinkSync(kpiFilename);
+          return res.status(200).send();
+        });
+
     } catch (e) {
       return next(boom.badRequest('Error while reading provided file'));
     }
-    try {
-      computedData = prepareDataForSavingToDb(fileContent);
-    } catch (e) {
-      console.log(e);
-      return next(boom.badRequest('Csv data are not in correct format'));
-    }
-    try {
-      await db.query('BEGIN');
-      const { startTime, itemStats, overview, sortedData } = computedData;
-      const item = await db.one(createNewItem(
-        scenarioName,
-        startTime,
-        environment,
-        note,
-        status,
-        projectName,
-        hostname));
-      await db.none(saveItemStats(item.id, JSON.stringify(itemStats), overview));
-      await db.none(saveKpiData(item.id, JSON.stringify(sortedData)));
-      await db.none(savePlotData(item.id, JSON.stringify(chunkData(sortedData))));
-      await db.query('COMMIT');
-      if (errors) {
-        const filename = errors[0].path;
-        const fileContent = fs.readFileSync(filename);
-        fs.unwatchFile(filename);
-        const jsonErrors = parser.toJson(fileContent);
-        await db.none(saveData(item.id, jsonErrors, ItemDataType.Error));
-      }
-      if (monitoring) {
-        const filename = monitoring[0].path;
-        const monitoringData = await csv().fromFile(filename);
-        const monitoringDataString = JSON.stringify(monitoringData);
-        fs.unwatchFile(filename);
-        await db.none(saveData(item.id, monitoringDataString, ItemDataType.MonitoringLogs));
-      }
-      res.status(200).send({
-        id: item.id,
-        overview,
-        status: Object.values(ItemStatus).find(_ => _ === status),
-      });
-    } catch (error) {
-      console.log(error);
-      await db.query('ROLLBACK');
-      return next(error);
-    }
-  });
+    // try {
+    //   console.log(`${Date.now()} data received....`)
+    //   computedData = prepareDataForSavingToDb(fileContent);
+    //   console.log(`${Date.now()} data prepared for db`);
+    // } catch (e) {
+    //   console.log(e);
+    //   return next(boom.badRequest('Csv data are not in correct format'));
+    // }
+    // try {
+    //   const { startTime, itemStats, overview, sortedData } = computedData;
+    //   const chunckedData = chunkData(sortedData);
+    //   console.log(`${Date.now()} data chunked`);
+    //   await db.query('BEGIN');
+    //   const item = await db.one(createNewItem(
+    //     scenarioName,
+    //     startTime,
+    //     environment,
+    //     note,
+    //     status,
+    //     projectName,
+    //     hostname));
+    //   console.log(`${Date.now()} stringifying...`);
+    //   await db.none(saveItemStats(item.id, JSON.stringify(itemStats), overview));
+    //   await db.none(saveKpiData(item.id, JSON.stringify(sortedData)));
+    //   await db.none(savePlotData(item.id, JSON.stringify(chunckedData)));
+    //   await db.query('COMMIT');
+    //   if (errors) {
+    //     const filename = errors[0].path;
+    //     const fileContent = fs.readFileSync(filename);
+    //     fs.unwatchFile(filename);
+    //     const jsonErrors = parser.toJson(fileContent);
+    //     await db.none(saveData(item.id, jsonErrors, ItemDataType.Error));
+    //   }
 
+
+    // if (monitoring) {
+    //   const filename = monitoring[0].path;
+    //   const monitoringData = await csv().fromFile(filename);
+    //   const monitoringDataString = JSON.stringify(monitoringData);
+    //   fs.unwatchFile(filename);
+    //   await db.none(saveData(item.id, monitoringDataString, ItemDataType.MonitoringLogs));
+    // }
+    //     res.status(200).send({
+    //       id: item.id,
+    //       overview,
+    //       status: Object.values(ItemStatus).find(_ => _ === status),
+    //     });
+    //   } catch (error) {
+    //     console.log(error);
+    //     await db.query('ROLLBACK');
+    //     return next(error);
+    //   }
+  });
 }
--- src/server/data-stats/prepare-data.ts
+++ src/server/data-stats/prepare-data.ts
@@ -1,6 +1,7 @@
 import { stats, itemOverview } from './statistics';
 
 export const prepareDataForSavingToDb = (inputData): ItemDbData => {
+    console.log(inputData)
     const sortedData = normalizeAndSortData(inputData);
     const itemStats = stats(sortedData);
     const overview = itemOverview(sortedData);
@@ -10,17 +11,22 @@ export const prepareDataForSavingToDb = (inputData): ItemDbData => {
 
 export const normalizeAndSortData = (inputData) => {
   return inputData.map(_ => {
-    _.timeStamp = parseInt(_.timeStamp, 10);
-    _.elapsed = parseInt(_.elapsed, 10);
-    _.responseCode = parseInt(_.responseCode, 10);
-    _.bytes = parseInt(_.bytes, 10);
-    _.grpThreads = parseInt(_.grpThreads, 10);
-    _.allThreads = parseInt(_.allThreads, 10);
-    _.Latency = parseInt(_.Latency, 10);
-    _.Connect = parseInt(_.Connect, 10);
-    return _;
+    return normalizeData(_);
   }).sort((curr, next) => {
     return curr.timeStamp - next.timeStamp; });
+};
+
+export const normalizeData = (_): OutputData => {
+  _.timeStamp = parseInt(_.timeStamp, 10);
+  _.elapsed = parseInt(_.elapsed, 10);
+  _.responseCode = parseInt(_.responseCode, 10);
+  _.bytes = parseInt(_.bytes, 10);
+  _.grpThreads = parseInt(_.grpThreads, 10);
+  _.allThreads = parseInt(_.allThreads, 10);
+  _.Latency = parseInt(_.Latency, 10);
+  _.Connect = parseInt(_.Connect, 10);
+  _.success = (_.success === 'true');
+  return _;
 };
 
 export interface ItemDbData {
@@ -28,4 +34,36 @@ export interface ItemDbData {
   overview: any;
   startTime: Date;
   sortedData: any;
+}
+
+export interface InputData {
+  timeStamp: string;
+  elapsed: string;
+  label: string;
+  responseCode: string;
+  responseMessage: string;
+  threadName: string;
+  success: string;
+  bytes: string;
+  grpThreads: string;
+  allThreads: string;
+  Latency: string;
+  Hostname: string;
+  Connect: string;
+};
+
+export interface OutputData {
+  timeStamp: number;
+  elapsed: number;
+  label: string;
+  responseCode: number;
+  responseMessage: string;
+  threadName: string;
+  success: boolean;
+  bytes: number;
+  grpThreads: number;
+  allThreads: number;
+  Latency: number;
+  Hostname: string;
+  Connect: number;
 }
