6cdcc3ea5c08ba6acabb6dfb8d56fb0266119bc6
Luděk Nový <ludek.novy@ceskatelevize.cz>
2020-08-31 15:25:51 +0200

    processing items endpoint

--- src/server/controllers/item/get-processing-items-controller.ts
+++ src/server/controllers/item/get-processing-items-controller.ts
@@ -2,9 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 
 import { getProcessingItems } from '../../queries/scenario';
 import { db } from '../../../db/db';
+import { ReportStatus } from '../../queries/items.model';
 
 export const getProcessingItemsController = async (req: Request, res: Response, next: NextFunction) => {
   const { projectName, scenarioName } = req.params;
   const processingItems = await db.any(getProcessingItems(projectName, scenarioName));
-  res.status(200).send(processingItems);
+  const inprogress = processingItems.filter((_) => _.reportStatus === ReportStatus.InProgress);
+  const failed = processingItems.filter((_) => _.reportStatus === ReportStatus.Error);
+  res.status(200).send({ failed, inprogress });
 };
