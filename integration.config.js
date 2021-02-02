e7dffa09961e3d11c37d43667801f2804926a323
Ludek Novy <ludeknovy@fastmail.com>
2021-02-02 18:23:25 +0100

    test commands updates

--- integration.config.js
+++ integration.config.js
@@ -8,7 +8,6 @@ module.exports = {
   'testMatch': [
     '**/src/tests/integration/**/*.spec.(ts|tsx|js)'
   ],
-  'testEnvironment': 'node',
   'verbose': true,
   'testEnvironment': '<rootDir>/src/tests/integration/helper/test-environment.js',
   'reporters': ['default']
