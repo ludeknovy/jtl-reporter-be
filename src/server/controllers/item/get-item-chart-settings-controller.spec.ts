fe874aaa280d0227bc575fa727fadd424d0abf80
Ludek Novy <ludeknovy@fastmail.com>
2021-04-03 10:18:47 +0200

    controller unit tests

---
+++ src/server/controllers/item/get-item-chart-settings-controller.spec.ts
@@ -0,0 +1,26 @@
+import { Response, NextFunction } from 'express';
+import { IGetUserAuthInfoRequest } from '../../middleware/request.model';
+import { getItemChartSettingsController } from './get-item-chart-settings-controller';
+jest.mock('../../../db/db');
+const mockResponse = () => {
+  const res: Partial<Response> = {};
+  res.send = jest.fn().mockReturnValue(res);
+  res.status = jest.fn().mockReturnValue(res);
+  return res;
+};
+
+describe('getItemChartSettingsController', () => {
+  it('should fetch data from db', async () => {
+    const nextFunction: NextFunction = jest.fn();
+    const response = mockResponse();
+    const querySpy = jest.spyOn(require('../../queries/items'), 'getItemChartSettings');
+    const request = {
+      params: { projectName: 'project', scenarioName: 'scenario', itemId: 'id' },
+      user: { userId: 'testUser' }
+    };
+    await getItemChartSettingsController(request as unknown as IGetUserAuthInfoRequest,
+      response as unknown as Response, nextFunction);
+    expect(querySpy).toHaveBeenCalledTimes(1);
+    expect(response.send).toHaveBeenCalledTimes(1);
+  });
+});
