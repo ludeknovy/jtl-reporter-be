f1f650f3df4acfe913bbd67016bde724647512a5
Ludek <13610612+ludeknovy@users.noreply.github.com>
2021-06-18 12:41:17 +0200

    delete share tokens cascade (#96)

---
+++ migrations/1624008038250_share-token-cascade-delete.js
@@ -0,0 +1,15 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+
+    pgm.dropConstraint({ schema: "jtl", name: "share_tokens" }, "share_tokens_item_id_fkey")
+
+    pgm.addConstraint({ schema: "jtl", name: "share_tokens" }, "share_tokens_item_id_fkey", {
+        foreignKeys: {
+            columns: "item_id",
+            references: { schema: "jtl", name: "items", column: "id" },
+            onDelete: 'CASCADE'
+        }
+    })
+};
+
+
