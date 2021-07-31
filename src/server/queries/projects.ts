58ae7eb1f8e19ef6193333e731e1a77b39edd048
Ludek Novy <ludeknovy@fastmail.com>
2021-07-31 09:54:44 +0200

    eslint fix

--- src/server/queries/projects.ts
+++ src/server/queries/projects.ts
@@ -35,7 +35,7 @@ export const findProjects = () => {
 
 export const latestItems = () => {
   return {
-    // tslint:disable-next-line:max-line-length
+    // eslint-disable-next-line max-len
     text: `SELECT i.id, s.name, environment, project_name as "projectName", stat.overview->'startDate' as "startTime", status FROM jtl.items as i
     LEFT JOIN jtl.scenario as s ON s.id = i.scenario_id
     LEFT JOIN jtl.projects as p ON p.id = s.project_id
