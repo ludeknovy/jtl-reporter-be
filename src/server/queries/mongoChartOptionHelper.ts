6c925603160db8da7d303d7031f82a317d405631
Ludek Novy <ludeknovy@fastmail.com>
2021-09-27 20:29:04 +0200

    removing mongo db aggregations

--- src/server/queries/mongoChartOptionHelper.ts
+++
@@ -1,5 +0,0 @@
-export const chartQueryOptionInterval = (duration: number): number => {
-  const durationMillis = duration * 60 * 1000;
-  const interval = parseInt((Math.floor(100 * (durationMillis / 200)) / 100).toFixed(0), 10);
-  return interval;
-};
