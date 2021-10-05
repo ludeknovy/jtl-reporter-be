66fcc124f1a0a14a020bd4f9999342b844be26fb
Ludek Novy <13610612+ludeknovy@users.noreply.github.com>
2021-10-05 20:10:51 +0200

    apdex (#110)

--- src/server/controllers/scenario/update-scenario-controller.ts
+++ src/server/controllers/scenario/update-scenario-controller.ts
@@ -5,7 +5,7 @@ import { updateScenario } from '../../queries/scenario';
 
 export const updateScenarioController = async (req: Request, res: Response, next: NextFunction) => {
   const { projectName, scenarioName } = req.params;
-  const { thresholds, analysisEnabled, scenarioName: name } = req.body;
-  await db.none(updateScenario(projectName, scenarioName, name, analysisEnabled, thresholds));
+  const { thresholds, analysisEnabled, scenarioName: name, apdex } = req.body;
+  await db.none(updateScenario(projectName, scenarioName, name, analysisEnabled, thresholds, apdex));
   res.status(204).send();
 };
