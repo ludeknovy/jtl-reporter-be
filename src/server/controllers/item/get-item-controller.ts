113aa2aa80cfbe4c0329a9a06c45138381e11bf9
Ludek Novy <ludeknovy@fastmail.com>
2021-10-07 17:45:52 +0200

    Revert "return apdex value in item data (#111)"
    This reverts commit d89a3d8bdc97c1bfcd4c38974095ba2dec03ca96.

--- src/server/controllers/item/get-item-controller.ts
+++ src/server/controllers/item/get-item-controller.ts
@@ -13,7 +13,7 @@ export const getItemController = async (req: Request, res: Response, next: NextF
     // eslint-disable-next-line @typescript-eslint/naming-convention
     base_id,
     status, hostname, reportStatus, thresholds,
-    analysisEnabled, apdex } = await db.one(findItem(itemId, projectName, scenarioName));
+    analysisEnabled } = await db.one(findItem(itemId, projectName, scenarioName));
   const { stats: statistics, overview, sutOverview } = await db.one(findItemStats(itemId));
 
   const monitoring: MonitoringData[] = await db.manyOrNone(getMonitoringData(itemId));
@@ -27,7 +27,7 @@ export const getItemController = async (req: Request, res: Response, next: NextF
   res.status(200).send({
     overview, sutOverview, statistics, status,
     plot, note, environment, hostname, reportStatus, thresholds, analysisEnabled,
-    baseId: base_id, isBase: base_id === itemId, apdex, monitoring: {
+    baseId: base_id, isBase: base_id === itemId, monitoring: {
       cpu: {
         data: monitoringAdjusted,
         max: maxCpu
