f76bc88120504796be494883715c51815ea74220
Ludek Novy <13610612+ludeknovy@users.noreply.github.com>
2021-11-23 08:32:54 +0100

    scenario network bytes combined (#130)
    Co-authored-by: Ludek Novy <ludeknovy@fastmail.com>

--- src/server/controllers/scenario/trends/get-scenario-trends-controller.ts
+++ src/server/controllers/scenario/trends/get-scenario-trends-controller.ts
@@ -4,6 +4,17 @@ import { scenarioTrends } from '../../../queries/scenario';
 
 export const getScenarioTrendsController = async (req: Request, res: Response, next: NextFunction) => {
   const { projectName, scenarioName } = req.params;
-  const ids = await db.any(scenarioTrends(projectName, scenarioName));
-  res.status(200).send(ids);
+  const data = await db.any(scenarioTrends(projectName, scenarioName));
+  const networkAdjustedData = data.map((_) => {
+    const { bytesPerSecond, bytesSentPerSecond } = _.overview;
+    const network = bytesPerSecond + bytesSentPerSecond;
+    return {
+      id: _.id,
+      overview: {
+        ..._.overview,
+        network
+      }
+    };
+  });
+  res.status(200).send(networkAdjustedData);
 };
