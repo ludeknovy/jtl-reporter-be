1cd02af98e95ccef7013a9e30f40e1ed048adefb
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2022-01-08 19:04:28 +0100

    type fix

--- custom-typings.d.ts
+++ custom-typings.d.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-unused-vars, no-underscore-dangle
 declare const __server__: unknown
 // eslint-disable-next-line no-unused-vars, no-underscore-dangle
-declare const __tokenHeaderKey__: unknown
+declare const __tokenHeaderKey__: string
