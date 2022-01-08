f5a0ec95b5d334a4abeb6afc6b263af6802a5c3a
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2022-01-08 19:31:47 +0100

    lint fix

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ import { NextFunction, Request, Response } from "express"
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const swaggerDocument = require("../openapi.json")
 
-const PORT = 5000;
+const PORT = 5000
 
 export class App {
   app: express.Application
