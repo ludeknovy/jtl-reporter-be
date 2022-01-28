742670f6678e5b22f595e63330cbb3c7a5d4fa2e
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2022-01-28 22:03:03 +0100

    lint fix

--- src/server/middleware/authorization-middleware.ts
+++ src/server/middleware/authorization-middleware.ts
@@ -3,7 +3,8 @@ import { logger } from "../../logger"
 
 export const authorizationMiddleware = (allowedRoles: AllowedRoles[]) => {
   return (request, response, next) => {
-    logger.info(`User ${request.user.userId} with role ${request.user.role} asking access to a resource with allowed roles: ${allowedRoles}`)
+    const user = request.user
+    logger.info(`User ${user.userId} with role ${user.role} accessing a resource with allowed roles: ${allowedRoles}`)
     if (allowedRoles.find((role) => role === request.user.role)) {
       // role is allowed, so continue on the next middleware
       next()
