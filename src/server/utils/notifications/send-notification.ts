17728b0efd3f216e11d834fe166a64fb38684faf
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2022-07-28 10:07:14 +0200

    gchat template

--- src/server/utils/notifications/send-notification.ts
+++ src/server/utils/notifications/send-notification.ts
@@ -1,42 +1,44 @@
-import { db } from "../../../db/db"
-import { scenarioNotifications } from "../../queries/scenario"
+import {db} from "../../../db/db"
+import {scenarioNotifications} from "../../queries/scenario"
 import axios from "axios"
-import { msTeamsTemplate } from "./templates/ms-teams-template"
-import { logger } from "../../../logger"
-import { linkUrl } from "./link-url"
-import { Overview } from "../../data-stats/prepare-data"
+import {msTeamsTemplate} from "./templates/ms-teams-template"
+import {logger} from "../../../logger"
+import {linkUrl} from "./link-url"
+import {Overview} from "../../data-stats/prepare-data"
+import {gchatTemplate} from "./templates/gchat-template";
 
 export const sendNotifications = async (projectName, scenarioName, id, overview: Overview) => {
-  try {
-    const notifications: Notification[] = await db.manyOrNone(scenarioNotifications(projectName, scenarioName))
-    const url = linkUrl(projectName, scenarioName, id)
-    notifications.map(async (notif) => {
-      const messageTemplate = NotificationTemplate.get(notif.type)
-      try {
-        logger.info(`sending notification to: ${notif.type}`)
-        const payload = messageTemplate(scenarioName, url, overview)
-        await axios.post(notif.url, payload, {
-          headers: {
-            "content-type": "application/json",
-          },
+    try {
+        const notifications: Notification[] = await db.manyOrNone(scenarioNotifications(projectName, scenarioName))
+        const url = linkUrl(projectName, scenarioName, id)
+        notifications.map(async (notif) => {
+            const messageTemplate = NotificationTemplate.get(notif.type)
+            try {
+                logger.info(`sending notification to: ${notif.type}`)
+                const payload = messageTemplate(scenarioName, url, overview)
+                await axios.post(notif.url, payload, {
+                    headers: {
+                        "content-type": "application/json"
+                    }
+                })
+            } catch (error) {
+                logger.error(`error while sending notification: ${error}`)
+            }
         })
-      } catch(error) {
-        logger.error(`error while sending notification: ${error}`)
-      }
-    })
-  } catch(error) {
-    logger.error(`Error notification processing: ${error}`)
-  }
+    } catch (error) {
+        logger.error(`Error notification processing: ${error}`)
+    }
 }
 
 
 interface Notification {
-  id: string
-  url: string
-  type: string
+    id: string
+    url: string
+    type: string
 }
 
 const NotificationTemplate = new Map([
-  ["ms-teams", msTeamsTemplate],
+    ["ms-teams", msTeamsTemplate],
+    ["gchat", gchatTemplate],
 ])
 
