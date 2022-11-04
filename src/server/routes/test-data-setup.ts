a5b65dd6f6b73aa4f5a9ea87961740d8e2942bf9
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2022-11-04 10:21:36 +0100

    uuid fixes

--- src/server/routes/test-data-setup.ts
+++ src/server/routes/test-data-setup.ts
@@ -13,7 +13,7 @@ import { createUserInDB } from "../controllers/users/create-new-user-controller"
 import { getUser } from "../queries/auth"
 import { generateToken } from "../controllers/auth/helper/token-generator"
 import { createNewApiToken } from "../queries/api-tokens"
-import * as uuid from "uuid"
+import { v4 as uuidv4 } from "uuid"
 import { ReportStatus } from "../queries/items.model"
 import { StatusCode } from "../utils/status-code"
 import { AllowedRoles } from "../middleware/authorization-middleware"
@@ -90,7 +90,7 @@ export class TestDataSetup {
       .post(
         wrapAsync(async (req: Request, res: Response) => {
           await db.any({ text: "TRUNCATE jtl.api_tokens CASCADE" })
-          const TOKEN = `at-${uuid()}`
+          const TOKEN = `at-${uuidv4()}`
           await createUserInDB("test-user", "test0000", AllowedRoles.Admin)
           const { id } = await db.one(getUser("test-user"))
           await db.any(createNewApiToken(TOKEN, "test-token", id))
