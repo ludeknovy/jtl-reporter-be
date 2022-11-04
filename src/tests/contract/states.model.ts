07f54663c107ab1af29b18c80b8b930266a3825f
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2022-11-04 09:57:51 +0100

    removing contract tests

--- src/tests/contract/states.model.ts
+++
@@ -1,9 +0,0 @@
-export enum States {
-  ExistingProject = "EXISTING_PROJECT",
-  ExistingScenario = "EXISTING_SCENARIO",
-  ExistingTestItem = "EXISTING_TEST_ITEM",
-  EmptyDb = "EMPTY_DB",
-  ExistingLogin = "EXISTING_LOGIN",
-  ExistingApiKey = "EXISTING_API_KEY",
-  NoUsers = "NO_USERS"
-}
