1d80228ecf497bf5502ef12202f3d0bb9c0317ae
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2023-01-07 21:17:02 +0100

    analytics

--- src/server/config.ts
+++ src/server/config.ts
@@ -2,4 +2,5 @@ export const config = {
   jwtToken: process.env.JWT_TOKEN,
   jwtTokenLogin: process.env.JWT_TOKEN_LOGIN,
   feUrl: process.env.FE_URL,
+  analyticsIdentifier: process.env.ANALYTICS_IDENTIFIER,
 }
