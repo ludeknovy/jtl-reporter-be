a00acb97a90f8f3646dada1a0fc43eade9234c24
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2023-05-09 09:00:51 +0200

    linter fixes

--- src/server/controllers/scenario/get-scenario-environment-controller.spec.ts
+++ src/server/controllers/scenario/get-scenario-environment-controller.spec.ts
@@ -2,7 +2,7 @@ import { Response } from "express"
 import { db } from "../../../db/db"
 import { getScenarioEnvironmentController } from "./get-scenario-environment-controller"
 import { IGetUserAuthInfoRequest } from "../../middleware/request.model"
-import {StatusCode} from "../../utils/status-code";
+import { StatusCode } from "../../utils/status-code"
 
 jest.mock("../../../db/db")
 const mockResponse = () => {
@@ -14,7 +14,7 @@ const mockResponse = () => {
 
 
 describe("getScenarioEnvironmentController", () => {
-    it("should return environments", async function() {
+    it("should return environments", async function () {
         const projectName = "my-project"
         const scenarioName = "my-scenario"
         const querySpy = jest.spyOn(require("../../queries/scenario"), "searchEnvironments");
