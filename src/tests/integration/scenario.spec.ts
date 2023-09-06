ddfb3e1092f9e09fe4110d8e6f0c105b6df9a665
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2023-09-06 20:10:31 +0200

    project not found tests for scenario tests

--- src/tests/integration/scenario.spec.ts
+++ src/tests/integration/scenario.spec.ts
@@ -36,6 +36,15 @@ describe("Scenario", () => {
                 .set("Accept", "application/json")
                 .expect(StatusCode.BadRequest)
         })
+        it("should return 404 when project does not exist", async () => {
+            await stateSetup(States.ExistingScenario)
+            await request(__server__)
+                .post("/api/projects/test-project-1/scenarios")
+                .set(__tokenHeaderKey__, credentials.token)
+                .send({ scenarioName: "test-scenario" })
+                .set("Accept", "application/json")
+                .expect(StatusCode.NotFound)
+        })
     })
     describe("PUT /projects/{projectName}/scenarios/{scenarioName}", () => {
         it("should be able to update scenario", async () => {
@@ -94,6 +103,62 @@ describe("Scenario", () => {
                 .set("Accept", "application/json")
                 .expect(StatusCode.NoContent)
         })
+        it("should return 404 when project does not exist", async () => {
+            await stateSetup(States.ExistingScenario)
+            await request(__server__)
+                .put("/api/projects/test-project-1/scenarios/test-scenario")
+                .set(__tokenHeaderKey__, credentials.token)
+                .send({
+                    scenarioName: "test-scenario",
+                    analysisEnabled: false,
+                    zeroErrorToleranceEnabled: true,
+                    deleteSamples: false,
+                    keepTestRunsPeriod: 7,
+                    generateShareToken: true,
+                    extraAggregations: false,
+                    thresholds: {
+                        enabled: true,
+                        percentile: 4.2,
+                        errorRate: 4.2,
+                        throughput: 8.3,
+                    },
+                    labelTrendChartSettings: {
+                        avgConnectionTime: true,
+                        avgLatency: true,
+                        avgResponseTime: true,
+                        errorRate: true,
+                        p90: true,
+                        p95: true,
+                        p99: true,
+                        throughput: false,
+                        virtualUsers: true,
+                    },
+                    userSettings: {
+                        requestStats: {
+                            samples: true,
+                            avg: true,
+                            min: true,
+                            max: true,
+                            p90: true,
+                            p95: true,
+                            p99: true,
+                            throughput: true,
+                            network: true,
+                            errorRate: true,
+                            failures: true,
+                            apdex: true,
+                            standardDeviation: true,
+                        },
+                    },
+                    apdexSettings: {
+                        toleratingThreshold: 200,
+                        satisfyingThreshold: 50,
+                        enabled: true,
+                    },
+                })
+                .set("Accept", "application/json")
+                .expect(StatusCode.NotFound)
+        })
     })
     describe("POST /projects/{projectName}/scenarios/{scenarioName}/trends/settings", () => {
         it("should be able to update scenario trends settings", async () => {
@@ -112,6 +177,22 @@ describe("Scenario", () => {
                 })
                 .expect(StatusCode.NoContent)
         })
+        it("should return 404 when project does not exist", async () => {
+            await stateSetup(States.ExistingScenario)
+            await request(__server__)
+                .post("/api/projects/test-project-1/scenarios/test-scenario/trends/settings")
+                .set(__tokenHeaderKey__, credentials.token)
+                .set("Accept", "application/json")
+                .send({
+                    aggregatedTrends: true,
+                    labelMetrics: {
+                        errorRate: true,
+                        throughput: true,
+                        percentile90: false,
+                    },
+                })
+                .expect(StatusCode.NotFound)
+        })
     })
     describe("DELETE /projects/{projectName}/scenarios/{scenarioName}", () => {
         it("should be able to delete scenario", async () => {
@@ -121,6 +202,14 @@ describe("Scenario", () => {
                 .set(__tokenHeaderKey__, credentials.token)
                 .set("Accept", "application/json")
                 .expect(StatusCode.NoContent)
+        })
+        it("should return 404 when project does not exist", async () => {
+            await stateSetup(States.ExistingScenario)
+            await request(__server__)
+                .delete("/api/projects/test-project-1/scenarios/test-scenario")
+                .set(__tokenHeaderKey__, credentials.token)
+                .set("Accept", "application/json")
+                .expect(StatusCode.NotFound)
         })
     })
 })
