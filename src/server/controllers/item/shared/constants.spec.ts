13aa0318db64291767e3af673abd3afcfda409b8
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2023-12-12 15:47:15 +0100

    ALLOWED_PERIOD test

---
+++ src/server/controllers/item/shared/constants.spec.ts
@@ -0,0 +1,8 @@
+import {ALLOWED_PERIOD} from "./constants";
+
+describe("ALLOWED_PERIOD", () => {
+    it("contains expected values", () => {
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        expect(ALLOWED_PERIOD).toEqual( [7, 14, 30, 90, 180] )
+    })
+})
