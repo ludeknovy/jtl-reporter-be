b1dcbb019d6a9553bfe9970b929bb4011dbdc86f
Ludek Novy <13610612+ludeknovy@users.noreply.github.com>
2024-01-22 16:59:58 +0100

    xlsx version upgraded (#283)

--- src/server/utils/excel-service.ts
+++ src/server/utils/excel-service.ts
@@ -4,9 +4,8 @@ export class ExcelService {
 
 
   static generateExcelBuffer(json: unknown[]): string {
-    const myworksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json)
-    const myworkbook: XLSX.WorkBook = { Sheets: { data: myworksheet }, SheetNames: ["data"] }
-    return XLSX.write(myworkbook, { bookType: "xlsx", bookSST: false, type: "base64" })
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json)
+    const workbook: XLSX.WorkBook = { Sheets: { data: worksheet }, SheetNames: ["data"] }
+    return XLSX.write(workbook, { bookType: "xlsx", bookSST: false, type: "base64" })
   }
-
 }
