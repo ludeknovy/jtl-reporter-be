ddc0bd5e551b53d91beb02cf9b002e901e4d719b
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2024-08-30 19:14:01 +0200

    Revert "Refactor Axios client initialization and request logic"
    This reverts commit 044db0b99d5dbe627b56b623cf4d261a07f098fb.

--- src/tests/integration/helper/state.ts
+++ src/tests/integration/helper/state.ts
@@ -4,47 +4,44 @@ import { Agent } from "http"
 const PROVIDER_URL = "http://localhost:5000"
 
 const options = (state, consumer): AxiosRequestConfig => {
-    return {
-        url: "/api/contract/states",
-        baseURL: PROVIDER_URL,
-        method: "POST",
-        data: {
-            consumer,
-            state,
-        },
-        headers: {
-            "Content-Type": "application/json",
-        },
-    }
+  return {
+    url: "/api/contract/states",
+    baseURL: PROVIDER_URL,
+    method: "POST",
+    data: {
+      consumer,
+      state,
+    },
+    headers: {
+      "Content-Type": "application/json",
+    },
+    httpAgent: new Agent({ keepAlive: false }),
+  }
 }
 
-const client = axios.create({
-    httpAgent: new Agent({ keepAlive: false }),
-})
-
 export const stateSetup = (state, consumer = "integration-tests") => {
-    return client.request(options(state, consumer))
+  return axios(options(state, consumer))
 }
 
 
 export const userSetup = (): Promise<AxiosResponse> => {
-    return axios({
-        url: "/api/contract/test-user",
-        baseURL: PROVIDER_URL,
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
+  return axios({
+    url: "/api/contract/test-user",
+    baseURL: PROVIDER_URL,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
 }
 
 export const apiTokenSetup = (): Promise<AxiosResponse> => {
-    return axios({
-        url: "/api/contract/api-token",
-        baseURL: PROVIDER_URL,
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
+  return axios({
+    url: "/api/contract/api-token",
+    baseURL: PROVIDER_URL,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
 }
