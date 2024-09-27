fc822e30f5815cd90a9a910d9e51bfefa93ac87c
Ludek Novy <13610612+ludeknovy@users.noreply.github.com>
2024-09-27 09:20:24 +0200

    Instance id (#342)

--- src/server/utils/analytics/anyltics-event.ts
+++ src/server/utils/analytics/anyltics-event.ts
@@ -1,44 +1,69 @@
 import { analytics } from "../analytics"
+import { db } from "../../../db/db"
+import { logger } from "../../../logger"
+import { v4 as uuidv4 } from "uuid"
+
+let INSTANCE_ID = null
+const FALLBACK_ID = uuidv4()
 
 export class AnalyticsEvent {
+
+    private static async getInstanceId(): Promise<string> {
+        if (INSTANCE_ID !== null) {
+            return INSTANCE_ID
+        }
+        try {
+            const result = await db.oneOrNone("SELECT instance FROM jtl.global")
+            if (result && result.instance) {
+                INSTANCE_ID = result.instance
+                return INSTANCE_ID
+            }
+                return FALLBACK_ID
+
+        } catch(error) {
+            logger.info("Instance id could not be loaded " + error)
+            return FALLBACK_ID
+        }
+
+    }
 
     static isAnalyticEnabled(): boolean {
         return !(process.env.OPT_OUT_ANALYTICS === "true")
     }
 
-    static reportProcessingStarted() {
+    static async reportProcessingStarted() {
         if (this.isAnalyticEnabled()) {
             analytics.track("reportProcessingStarted", {
                 // eslint-disable-next-line camelcase
-                distinct_id: process.env.ANALYTICS_IDENTIFIER,
+                distinct_id: await this.getInstanceId(),
             })
         }
     }
 
-    static reportProcessingFinished() {
+    static async reportProcessingFinished() {
         if (this.isAnalyticEnabled()) {
             analytics.track("reportProcessingFinished", {
                 // eslint-disable-next-line camelcase
-                distinct_id: process.env.ANALYTICS_IDENTIFIER,
+                distinct_id: await this.getInstanceId(),
             })
         }
     }
 
-    static reportDetails(labelCount, duration) {
+    static async reportDetails(labelCount, duration) {
         if (this.isAnalyticEnabled()) {
             analytics.track("reportInformation", {
                 // eslint-disable-next-line camelcase
-                distinct_id: process.env.ANALYTICS_IDENTIFIER,
+                distinct_id: await this.getInstanceId(),
                 labelCount,
                 duration,
             })
         }
     }
 
-    static reportUnexpectedError(error) {
+    static async reportUnexpectedError(error) {
         if (this.isAnalyticEnabled()) {
             analytics.track("unexpectedError", {
-                distinct_id: process.env.ANALYTICS_IDENTIFIER,
+                distinct_id: await this.getInstanceId(),
                 error,
             })
         }
