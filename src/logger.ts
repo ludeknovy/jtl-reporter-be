2f56211a8bd15d61b7ace61f6749f625304132b2
Dimitriys <dimitry-m@yandex.ru>
2024-11-19 14:57:33 +0300

    Fix timestamp format

--- src/logger.ts
+++ src/logger.ts
@@ -2,7 +2,7 @@ import * as winston from "winston"
 
 export const logger = winston.createLogger({
   format: winston.format.combine(
-    winston.format.timestamp({ format: "YYYY-MM-DD hh:mm:ss A" }),
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
     winston.format.json()
   ),
   levels: winston.config.syslog.levels,
