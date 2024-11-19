d24a17bff66dc1a8f71ad584ef470e4d41db2a25
zhq745w <dimitry-m@yandex.ru>
2024-11-19 15:51:04 +0300

    Add timestamp to log (#350)

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,10 @@
 import * as winston from "winston"
 
 export const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.json()
+  ),
   levels: winston.config.syslog.levels,
   transports: [ new winston.transports.Console() ],
 })
