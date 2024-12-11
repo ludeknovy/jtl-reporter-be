d60d8b23a606d1b791fc1ffa9c4e92fdebbc93ba
Ludek Novy <13610612+ludeknovy@users.noreply.github.com>
2024-12-11 10:10:43 +0100

    Add dynamic log level configuration with environment variable (#354)

--- src/logger.ts
+++ src/logger.ts
@@ -1,10 +1,32 @@
 import * as winston from "winston"
 
+const LOG_LEVELS = winston.config.syslog.levels
+const DEFAULT_LOG_LEVEL = "debug"
+
+const getLogLevel = (): string => {
+    const logLevelEnvVar = process.env.LOG_LEVEL
+    if (logLevelEnvVar) {
+        const isAllowedLogLevel = Object.keys(LOG_LEVELS)
+            .find(level => logLevelEnvVar.toLowerCase() === level.toLowerCase())
+        if (!isAllowedLogLevel) {
+            console.log("Unsupported log level: ", logLevelEnvVar)
+        } else {
+            return isAllowedLogLevel
+        }
+    }
+    return DEFAULT_LOG_LEVEL
+
+
+}
+
 export const logger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-    winston.format.json()
-  ),
-  levels: winston.config.syslog.levels,
-  transports: [ new winston.transports.Console() ],
+    format: winston.format.combine(
+        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+        winston.format.json()
+    ),
+    level: getLogLevel(),
+    levels: LOG_LEVELS,
+    transports: [new winston.transports.Console()],
 })
+
+
